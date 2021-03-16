7f3fc9b5264ddd769b9d4c1f98f73f7b33dbf511
Michael Hallam <michael.hallam1@outlook.com>
2021-03-16 20:29:15 +0000

    Changed the message

--- Digital Canvas/Program.cs
+++ Digital Canvas/Program.cs
@@ -10,7 +10,7 @@ namespace Digital_Canvas
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Test");
         }
     }
 }
