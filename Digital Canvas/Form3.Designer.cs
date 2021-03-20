04935022f0ea72b67a5c4973714bc90e21ecdfc4
HassnatSyed <syedhassnat.123@gmail.com>
2021-03-20 15:58:50 +0000

    penciol and canvas

---
+++ Digital Canvas/Form3.Designer.cs
@@ -0,0 +1,170 @@
+﻿
+namespace Digital_Canvas
+{
+    partial class Canvas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.colourPanel = new System.Windows.Forms.Panel();
+            this.green = new System.Windows.Forms.PictureBox();
+            this.blue = new System.Windows.Forms.PictureBox();
+            this.red = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mainToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.colourPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.green)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.red)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BackColor = System.Drawing.Color.White;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 24);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.pictureBox1);
+            this.splitContainer1.Panel2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.splitContainer1_Panel2_MouseDown);
+            this.splitContainer1.Panel2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.splitContainer1_Panel2_MouseMove);
+            this.splitContainer1.Panel2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.splitContainer1_Panel2_MouseUp);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 426);
+            this.splitContainer1.SplitterDistance = 101;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(500, -21);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(20, 18);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // colourPanel
+            // 
+            this.colourPanel.BackColor = System.Drawing.Color.Gainsboro;
+            this.colourPanel.Controls.Add(this.green);
+            this.colourPanel.Controls.Add(this.blue);
+            this.colourPanel.Controls.Add(this.red);
+            this.colourPanel.Location = new System.Drawing.Point(602, 0);
+            this.colourPanel.Name = "colourPanel";
+            this.colourPanel.Size = new System.Drawing.Size(186, 24);
+            this.colourPanel.TabIndex = 0;
+            // 
+            // green
+            // 
+            this.green.BackColor = System.Drawing.Color.Lime;
+            this.green.Location = new System.Drawing.Point(90, 3);
+            this.green.Name = "green";
+            this.green.Size = new System.Drawing.Size(20, 18);
+            this.green.TabIndex = 3;
+            this.green.TabStop = false;
+            this.green.Click += new System.EventHandler(this.red_Click);
+            // 
+            // blue
+            // 
+            this.blue.BackColor = System.Drawing.Color.Blue;
+            this.blue.Location = new System.Drawing.Point(64, 3);
+            this.blue.Name = "blue";
+            this.blue.Size = new System.Drawing.Size(20, 18);
+            this.blue.TabIndex = 4;
+            this.blue.TabStop = false;
+            this.blue.Click += new System.EventHandler(this.red_Click);
+            // 
+            // red
+            // 
+            this.red.BackColor = System.Drawing.Color.Red;
+            this.red.Location = new System.Drawing.Point(38, 3);
+            this.red.Name = "red";
+            this.red.Size = new System.Drawing.Size(20, 18);
+            this.red.TabIndex = 5;
+            this.red.TabStop = false;
+            this.red.Click += new System.EventHandler(this.red_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mainToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mainToolStripMenuItem
+            // 
+            this.mainToolStripMenuItem.Name = "mainToolStripMenuItem";
+            this.mainToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.mainToolStripMenuItem.Text = "Main";
+            // 
+            // Canvas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.colourPanel);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Canvas";
+            this.Text = "Form3";
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.colourPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.green)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.red)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel colourPanel;
+        private System.Windows.Forms.PictureBox green;
+        private System.Windows.Forms.PictureBox blue;
+        private System.Windows.Forms.PictureBox red;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mainToolStripMenuItem;
+    }
+}
---
+++ Digital Canvas/Form3.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Digital_Canvas
+{
+    public partial class Canvas : Form
+    {
+        //creating 
+        Graphics graphic;
+        int x = -1;
+        int y = -1;
+        bool moving = false;
+        Pen pencil;
+        public Canvas()
+        {
+            InitializeComponent();
+            graphic = splitContainer1.Panel2.CreateGraphics();
+            pencil = new Pen(Color.Black,5);
+        }
+
+        private void red_Click(object sender, EventArgs e)
+        {
+            PictureBox colours = (PictureBox)sender;
+            pencil.Color = colours.BackColor; // sets the selected colour
+        }
+
+        private void splitContainer1_Panel2_MouseDown(object sender, MouseEventArgs e)
+        {
+            moving = true;
+            x = e.X; // gets mouse x location
+            y = e.Y; // gets mouse  y location
+
+        }
+
+        private void splitContainer1_Panel2_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (moving && x!=-1 && y != -1)
+            {
+                graphic.DrawLine(pencil, new Point(x, y), e.Location);
+            }
+        }
+
+        private void splitContainer1_Panel2_MouseUp(object sender, MouseEventArgs e)
+        {
+            moving = false;
+            x = -1;
+            x = -1;
+        }
+    }
+}
