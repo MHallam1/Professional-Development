18b9dd463039c90c01ad6fb108338e7fb11e50d9
HassnatSyed <syedhassnat.123@gmail.com>
2021-03-20 16:14:10 +0000

    now runs as application

--- Digital Canvas/Program.cs
+++ Digital Canvas/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Digital_Canvas
 {
@@ -10,6 +11,10 @@ namespace Digital_Canvas
     {
         static void Main(string[] args)
         {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Canvas());
+
             Console.WriteLine("Test");
             Console.WriteLine("meme meme");
             Console.WriteLine("meme meme");
